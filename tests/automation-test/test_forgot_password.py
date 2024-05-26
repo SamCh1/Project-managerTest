5059f4f9962a34975df5d90c2955b89d60478cc6
NghiemXuan11 <11112001nghiemit@gmail.com>
2024-05-27 00:52:30 +0700

    Last Update 27/5/2024

--- tests/automation-test/test_forgot_password.py
+++ tests/automation-test/test_forgot_password.py
@@ -33,39 +33,39 @@ class TestForgotPasswordFunction(unittest.TestCase):
 
     # Happy Cases
     # TC_23
-    # def test23(self):
-    #     with open("Data/forgot_password_function_data.json", encoding="utf-8") as f:
-    #         data = json.load(f)
-    #         email = data["test_cases_Forgot_Password"][0]["email"]
-    #         password = data["test_cases_Forgot_Password"][0]["password"]
-    #         self.login(email)
-    #         time.sleep(5)
-    #         # Lấy OTP
-    #         unreadThreads = ezgmail.unread()
-    #         a = str(unreadThreads[0])
-    #         otpmatch = regex.search(r'\b\d{8}\b', a)
-    #         otp = otpmatch.group(0)
-    #         # print(a)
-    #         # print(otp)
-    #         time.sleep(6)
-    #         # Điền OTP và đổi mật khẩu
-    #         self.driver.find_element(By.ID, "otp").send_keys(otp)
-    #         self.driver.find_element(
-    #             By.CSS_SELECTOR, "body > div > div > div > form > div:nth-child(3) > button").click()
-    #         self.driver.find_element(By.ID, "password").send_keys(password)
-    #         self.driver.find_element(
-    #             By.ID, "confirmPassword").send_keys(password)
-    #         self.driver.find_element(
-    #             By.CSS_SELECTOR, "body > div.container.my-5 > div > div > form > div:nth-child(3) > button").click()
+    def test23(self):
+        with open("Data/forgot_password_function_data.json", encoding="utf-8") as f:
+            data = json.load(f)
+            email = data["test_cases_Forgot_Password"][0]["email"]
+            password = data["test_cases_Forgot_Password"][0]["password"]
+            self.login(email)
+            time.sleep(5)
+            # Lấy OTP
+            unreadThreads = ezgmail.unread()
+            a = str(unreadThreads[0])
+            otpmatch = regex.search(r'\b\d{8}\b', a)
+            otp = otpmatch.group(0)
+            # print(a)
+            # print(otp)
+            time.sleep(6)
+            # Điền OTP và đổi mật khẩu
+            self.driver.find_element(By.ID, "otp").send_keys(otp)
+            self.driver.find_element(
+                By.CSS_SELECTOR, "body > div > div > div > form > div:nth-child(3) > button").click()
+            self.driver.find_element(By.ID, "password").send_keys(password)
+            self.driver.find_element(
+                By.ID, "confirmPassword").send_keys(password)
+            self.driver.find_element(
+                By.CSS_SELECTOR, "body > div.container.my-5 > div > div > form > div:nth-child(3) > button").click()
 
-    #         success_message_element = WebDriverWait(self.driver, 10).until(
-    #             EC.visibility_of_element_located(
-    #                 (By.CLASS_NAME, "alert-success"))
-    #         )
-    #         success_message = success_message_element.text.strip()
-    #         # self.driver.save_screenshot('123lmss.png')
-    #         print(success_message)
-    #         self.assertEqual(success_message, "Đổi mật khẩu thành công! x")
+            success_message_element = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(
+                    (By.CLASS_NAME, "alert-success"))
+            )
+            success_message = success_message_element.text.strip()
+            # self.driver.save_screenshot('123lmss.png')
+            print(success_message)
+            self.assertEqual(success_message, "Đổi mật khẩu thành công! x")
 
     # Negative Cases
     # TC_24
@@ -397,42 +397,42 @@ class TestForgotPasswordFunction(unittest.TestCase):
     #             error_message, "password: Phải có ít nhất 2 chữ số x")
 
     # TC_35
-    def test35(self):
-        with open("Data/forgot_password_function_data.json", encoding="utf-8") as f:
-            data = json.load(f)
-            email = data["test_cases_Forgot_Password"][12]["email"]
-            password = data["test_cases_Forgot_Password"][12]["password"]
-            confirmPassword = data["test_cases_Forgot_Password"][12]["confirmpassword"]
-            self.login(email)
-            time.sleep(5)
-            # Lấy OTP
-            unreadThreads = ezgmail.unread()
-            a = str(unreadThreads[0])
-            otpmatch = regex.search(r'\b\d{8}\b', a)
-            otp = otpmatch.group(0)
-            # print(a)
-            # print(otp)
-            time.sleep(6)
-            # Điền OTP và đổi mật khẩu
-            self.driver.find_element(By.ID, "otp").send_keys(otp)
-            self.driver.find_element(
-                By.CSS_SELECTOR, "body > div > div > div > form > div:nth-child(3) > button").click()
-            self.driver.find_element(
-                By.ID, "password").send_keys(password)
-            self.driver.find_element(
-                By.ID, "confirmPassword").send_keys(confirmPassword)
-            self.driver.find_element(
-                By.CSS_SELECTOR, "body > div.container.my-5 > div > div > form > div:nth-child(3) > button").click()
+    # def test35(self):
+    #     with open("Data/forgot_password_function_data.json", encoding="utf-8") as f:
+    #         data = json.load(f)
+    #         email = data["test_cases_Forgot_Password"][12]["email"]
+    #         password = data["test_cases_Forgot_Password"][12]["password"]
+    #         confirmPassword = data["test_cases_Forgot_Password"][12]["confirmpassword"]
+    #         self.login(email)
+    #         time.sleep(5)
+    #         # Lấy OTP
+    #         unreadThreads = ezgmail.unread()
+    #         a = str(unreadThreads[0])
+    #         otpmatch = regex.search(r'\b\d{8}\b', a)
+    #         otp = otpmatch.group(0)
+    #         # print(a)
+    #         # print(otp)
+    #         time.sleep(6)
+    #         # Điền OTP và đổi mật khẩu
+    #         self.driver.find_element(By.ID, "otp").send_keys(otp)
+    #         self.driver.find_element(
+    #             By.CSS_SELECTOR, "body > div > div > div > form > div:nth-child(3) > button").click()
+    #         self.driver.find_element(
+    #             By.ID, "password").send_keys(password)
+    #         self.driver.find_element(
+    #             By.ID, "confirmPassword").send_keys(confirmPassword)
+    #         self.driver.find_element(
+    #             By.CSS_SELECTOR, "body > div.container.my-5 > div > div > form > div:nth-child(3) > button").click()
 
-            error_message_element = WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located(
-                    (By.CLASS_NAME, "alert-success"))
-            )
-            error_message = error_message_element.text.strip()
-            self.driver.save_screenshot('image/fail/TC_35.jpg')
-            print(error_message)
-            self.assertEqual(
-                error_message, "password: Độ dài mật khẩu không quá 100 ký tự x")
+    #         error_message_element = WebDriverWait(self.driver, 10).until(
+    #             EC.visibility_of_element_located(
+    #                 (By.CLASS_NAME, "alert-success"))
+    #         )
+    #         error_message = error_message_element.text.strip()
+    #         self.driver.save_screenshot('image/fail/TC_35.jpg')
+    #         print(error_message)
+    #         self.assertEqual(
+    #             error_message, "password: Độ dài mật khẩu không quá 100 ký tự x")
 
 
 if __name__ == "__main__":
